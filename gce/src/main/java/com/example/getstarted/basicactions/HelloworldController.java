55b480ccddb6a4c2f4cb8b6d0ae391c4c9bf5503
Juan Carlos <jackcreativa@gmail.com>
2023-11-05 19:01:41 -0600

    adding log4j

--- gce/src/main/java/com/example/getstarted/basicactions/HelloworldController.java
+++ gce/src/main/java/com/example/getstarted/basicactions/HelloworldController.java
@@ -21,13 +21,34 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+//import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @WebServlet(value = "/")
 public class HelloworldController extends HttpServlet {
+//static Logger log = Logger.getLogger(HelloworldController.class.getName());
+
 
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    resp.getWriter().write("Hello world - GCE! I am here   ");
+
+    Logger logger = LogManager.getRootLogger();
+    logger.trace("Configuration File Defined To Be :: "+System.getProperty("log4j.configurationFile"));
+    logger.info("Calling Method Run_10K");
+    logger.info("Calling Method Run_20K");
+    //   logger.printf("Integer.MAX_VALUE = %,d", Integer.MAX_VALUE);
+    logger.debug("Long.MAX_VALUE = %,d", Long.MAX_VALUE);
+    logger.trace("Entering method processOrder().");
+    logger.debug("Received order with ID 12345.");
+    logger.info("Order shipped successfully.");
+    logger.warn("Potential security vulnerability detected in user input: '...'");
+    logger.error("Failed to process order. Error: {. . .}");
+    logger.fatal("System crashed. Shutting down...");
+
+    resp.getWriter().write("Hello world - GCE i am in again! new version");
     resp.setStatus(HttpServletResponse.SC_OK);
+
+
   }
 }
